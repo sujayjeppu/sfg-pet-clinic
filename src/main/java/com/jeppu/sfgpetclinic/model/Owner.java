c4a3961850c9a28180c9f985983c6a3212434a83
Sujay Jeppu <sujay.jeppu@gmail.com>
2021-08-28 09:48:40 +0530

    Adding POJOs. Closes #2.

---
+++ src/main/java/com/jeppu/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.jeppu.sfgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/jeppu/sfgpetclinic/model/PetType.java
@@ -0,0 +1,20 @@
+package com.jeppu.sfgpetclinic.model;
+
+public class PetType {
+    private String name;
+
+    public PetType() {
+    }
+
+    public PetType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
