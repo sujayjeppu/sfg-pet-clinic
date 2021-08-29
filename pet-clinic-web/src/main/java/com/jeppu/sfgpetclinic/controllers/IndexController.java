12506386c37ba761bbe9e0fc9a958fc3b04e3e65
Sujay Jeppu <sujay.jeppu@gmail.com>
2021-08-29 20:15:07 +0530

    Create Pet Clinic Controllers. Closes #7.

---
+++ pet-clinic-web/src/main/java/com/jeppu/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,12 @@
+package com.jeppu.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping(value = {"", "/", "index", "index.html"})
+    public String index(){
+        return "index";
+    }
+}
